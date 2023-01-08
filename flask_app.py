98b7cac3152b03bfd0e05d12abd7b519a3f8b02c
Arthur O'Leary <arthur.oleary24@mail.dcu.ie>
2023-01-08 22:26:38 +0000

    completed app

--- flask_app.py
+++ flask_app.py
@@ -1,29 +1,45 @@
-from flask import Flask, request, render_template, session, redirect
-import numpy as np
-import pandas as pd
+from flask import Flask, request, render_template
+
 import yfinance as yf
 
 
 
 app = Flask(__name__)
 
-msft = yf.Ticker("MSFT")
-# show sustainability
 
-
-df = msft.sustainability
-
-# pd.DataFrame({'A': [0, 1, 2, 3, 4],
-#                    'B': [5, 6, 7, 8, 9],
-#                    'C': ['a', 'b', 'c--', 'd', 'e']})
-
-
-@app.route('/', methods=("POST", "GET"))
+@app.route('/')
 def index():
+    return render_template('index.html')
 
-    return render_template('index.html',  tables=[df.to_html(classes='data')], titles=df.columns.values)
+@app.route('/error/')
+def error():
+    return render_template('error.html')
+
+@app.route('/result/')
+def result():
+    return render_template('result.html')
+
+@app.route('/about/')
+def about():
+    return render_template('about.html')
 
 
+
+@app.route('/search/', methods =["GET", "POST"])
+def search():
+  try:
+    if request.method == "POST":
+
+      ticker = request.form['ticker']
+      symbol = request.args.get('symbol', ticker)
+
+      quote = yf.Ticker(symbol)
+
+      return render_template("result.html", quote=quote)
+  except Exception as error:
+    # Code to handle the error goes here
+    return render_template("error.html", error=error)
+  return render_template("result.html")
 
 if __name__ == '__main__':
-    app.run(debug=False)
+    app.run()
